e2dc1c99efc9813e676f4c8b0d656fe981a555c6
LordEnio <enioh@cox.net>
2015-04-12 21:42:26 -0700

    Update status.h

---
+++ status.h
@@ -0,0 +1,14 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+class Player
+{
+    public:
+        Player();
+        int health;
+        int maxHealth;
+        int dmg;
+        int suspicion;
+        int score;
+};
+#endif // STATUS_H
