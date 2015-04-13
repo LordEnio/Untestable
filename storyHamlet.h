ea585d42f7f6676386d43895f0e0b3f78cafbbdb
LordEnio <enioh@cox.net>
2015-04-12 21:43:28 -0700

    Update storyHamlet.h

--- storyHamlet.h
+++ storyHamlet.h
@@ -1,11 +1,7 @@
 #ifndef STORYHAMLET_H
 #define STORYHAMLET_H
 
-int storyNum;   ///Where are you in the plot?
-bool storyOn;   ///Is the game plot still moving forward?
-
 void printIntro();
-int storyDialogue();
-void printScr();
+int storyDialogue(int);
 
 #endif // STORYHAMLET_H
