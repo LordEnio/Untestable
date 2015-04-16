943d43c9588ec1f3b7f415585d987262a26b5a0f
Boomshaka <shakakanenobu@yahoo.com>
2015-04-15 20:42:08 -0700

    Update status.cpp

--- Untestable/status.cpp
+++ Untestable/status.cpp
@@ -9,4 +9,25 @@ Player::Player()
     score = 0;
 }
 
+unsigned int nxtLevel (unsigned int level)
+{
+    return 16*level*level;
+}
+
+
+bool levelUp ()
+{
+    if (this -> exp >= nxtLevel(level+1))
+    {
+        ++level;
+        
+        unsigned int healthUp = 0;
+        unsigned int dmgUp = 0;
+        
+        this -> healthUp = healthUp + level*level + rand() % 4;
+        this -> dmgUp = dmgUp + 1 + rand() % 3;
+        return true;
+    }
+    return false;
+}
 
